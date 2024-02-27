78f8e68062e7132a276e09d7eefbe0a2ca4de25d
rustamkutovyi <rustamkutovoy996@gmail.com>
2024-02-26 22:08:25 -0800

    tests fot intercept have beeb reforamted

--- cypress/e2e/inercept.cy.ts
+++ cypress/e2e/inercept.cy.ts
@@ -6,20 +6,8 @@ describe('INTERCEPT', () => {
     })
     
     it('Intercept for Login', function(){
-        // cy.fixture('progress.json').as('data')
-        // cy.fixture('katas.json').as('resBody')
         cy.intercept('POST', '*/login').as('login')
-        // cy.intercept('https://server-stage.pasv.us/course/coursesProgress/65d80a21d34bb09828b7df5e',
-        //  {fixture: 'progress.json'}
-        // ).as('course')
-        // cy.intercept('https://server-stage.pasv.us/progress/codewars/completed/65d80a21d34bb09828b7df5e',
-        // {fixture: 'katas.json'}
-        // ).as('katas')
-
-        // cy.visit(`${Cypress.env('stage')}/user/login`)
-        // cy.get('#normal_login_email').type(Cypress.env('email'))
-        // cy.get('#normal_login_password').type(Cypress.env('password'), { log: false })
-        // cy.get('button[type="submit"]').click()
+     
         cy.wait('@login').then(res => {
             console.log(res, 'response');
             let id = res.response.body.payload.user._id
@@ -29,19 +17,6 @@ describe('INTERCEPT', () => {
                 expect(res.response.statusCode).to.eq(200)
             })
         })
-        // cy.visit('https://stage.pasv.us/profile/65d80a21d34bb09828b7df5e/progress')
-        // cy.wait('@course').then(el=> {
-        //     console.log(el, 'response');
-            
-        //     cy.wrap(this.data).should('deep.equal', el.response.body)
-        // })
-
-        // cy.visit('https://stage.pasv.us/profile/65d80a21d34bb09828b7df5e/katas')
-        // cy.wait('@katas').then(res => {
-        //   console.log(res, 'katasResponse');
-
-        //   cy.wrap(this.resBody).should('deep.equal', res.response.body)
-        // })
     });
 
     it('Intercept for Progress', function() {
@@ -51,7 +26,7 @@ describe('INTERCEPT', () => {
         ).as('course')
         cy.visit('https://stage.pasv.us/profile/65d80a21d34bb09828b7df5e/progress')
         cy.wait('@course').then(el=> {
-            console.log(el, 'response');
+            console.log(el, 'progressResponse');
             
             cy.wrap(this.data).should('deep.equal', el.response.body)
         })
